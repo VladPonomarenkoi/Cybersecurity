d5b5d3f93d5b65e8a12ec360f16f3b9eecfb6d5c
VladPonomarenkoi <gggrrry392@gmail.com>
2021-12-01 07:40:56 +0200

    first commit

---
+++ lab3-4/lab3x3/lab3x3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace _3part
+{
+    class Program
+    {
+        public static byte[] ComputeHmacsha1(byte[] toBeHashed, byte[] key)
+        {
+            using (var hmac = new HMACSHA1(key))
+            {
+                return hmac.ComputeHash(toBeHashed);
+            }
+        }
+        public static void CheckIfConfident(string GettedMessage, string key, string GettedHash)
+        {
+            Console.WriteLine("Обчислення та порiвняння хешування");
+            var tempComputedHash = ComputeHmacsha1(Encoding.Unicode.GetBytes(GettedMessage), Encoding.Unicode.GetBytes(key));
+            if (GettedHash == Convert.ToBase64String(tempComputedHash))
+            {
+                Console.WriteLine("Обчислений хеш той самий, що й отриманий");
+                Console.WriteLine("Авторизовано");
+            }
+            else
+            {
+                Console.WriteLine("Обчислений хеш НЕ той самий, що отриманий");
+                Console.WriteLine("НЕ Авторизовано");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            const string strForHash = "Hello World!";
+            const string IncorectStrForHash = "Buy World!";
+            const string keyForHash = "secret";
+            const string HashWeGet = "+wS72Kx9qyWwG1LhKL4rDbgbNqc=";
+
+            var sha1ForStr = ComputeHmacsha1(Encoding.Unicode.GetBytes(strForHash), Encoding.Unicode.GetBytes(keyForHash));
+            Console.WriteLine($"Hash sha1:{Convert.ToBase64String(sha1ForStr)}");
+            Console.WriteLine("-------------------");
+            CheckIfConfident(strForHash, keyForHash, HashWeGet);
+            Console.WriteLine("-------------------");
+            CheckIfConfident(IncorectStrForHash, keyForHash, HashWeGet);
+
+
+        }
+    }
+}
---
+++ lab3-4/lab3x4/lab3x4/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+
+namespace lab3х4
+{
+    class Program
+    {
+        public static byte[] ComputeHashSha256(byte[] toBeHashed)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(toBeHashed);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<string> logins = new List<string>();
+            List<string> passwords = new List<string>();
+
+            while (true)
+            {
+
+                Console.Write("Enter '1' to logIN and '0' to logUP: ");
+                var temp = Console.ReadLine();
+                if (temp == "1")
+                {
+                    Console.Write("Будь ласка, введiть свiй логiн: ");
+                    var login = Console.ReadLine();
+                    var sha256ForStr1 = Convert.ToBase64String(ComputeHashSha256(Encoding.Unicode.GetBytes(login)));
+                    if (logins.Contains(sha256ForStr1))
+                    {
+                        var ind = logins.IndexOf(sha256ForStr1);
+                        Console.Write("Будь ласка, введiть свiй пароль: ");
+                        var pass = Console.ReadLine();
+                        var sha256Forpass = Convert.ToBase64String(ComputeHashSha256(Encoding.Unicode.GetBytes(pass)));
+                        if (passwords[ind] == sha256Forpass)
+                        {
+                            Console.WriteLine("Ви увiйшли");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неправильний пароль");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неправильний логiн");
+                    }
+
+
+
+                }
+                else if (temp == "0")
+                {
+                    Console.Write("Будь ласка, введiть свiй логiн: ");
+                    var login = Console.ReadLine();
+                    var sha256ForStr1 = Convert.ToBase64String(ComputeHashSha256(Encoding.Unicode.GetBytes(login)));
+                    if (!logins.Contains(sha256ForStr1))
+                    {
+                        logins.Add(sha256ForStr1);
+                        Console.Write("Будь ласка, введiть свiй пароль: ");
+                        var pass = Console.ReadLine();
+                        var sha256Forpass = Convert.ToBase64String(ComputeHashSha256(Encoding.Unicode.GetBytes(pass)));
+                        passwords.Add(sha256Forpass);
+                        Console.WriteLine("Ви зареєструвалися");
+                    }
+                    else
+                    {
+                        Console.WriteLine("логiн вже в базi");
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Введiть правильно, будь ласка");
+                }
+            }
+        }
+    }
+}
+
